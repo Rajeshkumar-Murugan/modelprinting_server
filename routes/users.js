7b03db6156f67aab3bb526b8532b2473c4d10259
Rajeshkumar-Murugan <rajeshkumarmurugan1@gmail.com>
2022-04-12 15:54:57 +0530

    firstcommit

--- routes/users.js
+++ routes/users.js
@@ -65,9 +65,10 @@ router.post('/register', async(req, res, next)=> {
         to: account.email,
         subject: 'Verification token',
         html: `
-        
+
         <img src="https://cdn.dribbble.com/users/1238709/screenshots/4069900/success_celebration_800x600.gif"><br/>
        <a href ="https://modelprintingserver.herokuapp.com/users/verify-token/${token}" method="get">Click Here</a> to verify your account.
+       <br/>
        <b>Note: <b><p>Link will be valid only for 5mins</p>
         `                
       };
